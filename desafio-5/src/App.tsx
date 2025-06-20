721813428e72285ae7abdb12334ce741be18ec32
Sthefane <sthefanedias04@gmail.com>
2025-06-19 21:13:03 -0300

    feat: criação da navbar e estilização da pagina inicial

--- desafio-5/src/App.tsx
+++ desafio-5/src/App.tsx
@@ -4,9 +4,19 @@ function App() {
 
   return (
     <>
-      <div>
+    <main className='font-[Poppins, sans-serif]'>
+      <header className='header-style flex justify-center gap-4 p-4 bg-[#454955] text-white md:text-sm font-medium'>
+        <a href="">Início</a>
+        <a href="">Sobre</a>
+        <a href="">Artigos</a>
+        <a href="">Contato</a>
+        <a href="">Login</a>
+      </header>
+
+      <div className='body-style flex flex-col items-center justify-center h-screen bg-[#F3EFF5]'>
         <h1>Hello World</h1>
       </div>
+    </main>
     </>
   )
 }
