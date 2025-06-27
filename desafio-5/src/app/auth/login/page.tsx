d279747f52b5abc9d3fa6742e260e37108069896
alice torres <torresalice.dev@gmail.com>
2025-06-26 21:13:39 -0300

    editando pagina login

--- desafio-5/src/app/auth/login/page.tsx
+++ desafio-5/src/app/auth/login/page.tsx
@@ -1,7 +1,31 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './login.css'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="login-container">
+      <h2>Faça seu login em nossa plataforma!</h2>
+
+      <div className="login-box">
+        <h3>Entrar</h3>
+
+        <form>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email"  required />
+
+          <label htmlFor="senha">Senha:</label>
+          <input type="password" id="senha"  required />
+
+          <button type="submit">Entrar</button>
+        </form>
+
+        <div className="login-links">
+          <Link to="#">Esqueci a senha</Link>
+          <Link to="/signup">Não tenha cadastro</Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
