e314e8bf04bdd1505a018208fa6c5100fe5f074e
Sthefane <sthefanedias04@gmail.com>
2025-06-27 22:15:45 -0300

    feat: criação e implementação da pagina de artigos

---
+++ desafio-5/src/app/artigos/_components/carousel.tsx
@@ -0,0 +1,88 @@
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Link } from "react-router-dom";
+
+interface CarouselProps {
+  className?: string;
+  props: {
+    title: string;
+    image: string;
+    description: string;
+    href: string;
+  };
+}
+
+const CarrosselCard = ({ props }: CarouselProps) => {
+  return (
+    <Card className="flex flex-col items-center justify-center h-full gap-4 px-8">
+      <img
+        src={props.image}
+        alt={props.title}
+        className="w-full h-48 object-cover rounded-lg"
+      />
+      <h2 className="text-2xl font-bold w-full text-left">{props.title}</h2>
+      <p className="mt-2 text-gray-600 w-full text-left">{props.description}</p>
+      <Button variant="default" asChild>
+        <Link to={props.href}>Ler mais</Link>
+      </Button>
+    </Card>
+  );
+};
+
+const Carrossel = () => {
+  const articles = [
+    {
+      title: "Artigo 1",
+      image: "https://thumbs.dreamstime.com/b/wallpaper-de-fundo-cinza-claro-abstrato-vazio-sala-est%C3%BAdio-usada-para-exibi%C3%A7%C3%A3o-produto-e-site-templatebackground-exibir-o-254939223.jpg",
+      description: "Descrição do artigo 1",
+      href: "#",
+    },
+    {
+      title: "Artigo 2",
+      image: "https://thumbs.dreamstime.com/b/wallpaper-de-fundo-cinza-claro-abstrato-vazio-sala-est%C3%BAdio-usada-para-exibi%C3%A7%C3%A3o-produto-e-site-templatebackground-exibir-o-254939223.jpg",
+      description: "Descrição do artigo 2",
+      href: "#",
+    },
+    {
+      title: "Artigo 3",
+      image: "https://thumbs.dreamstime.com/b/wallpaper-de-fundo-cinza-claro-abstrato-vazio-sala-est%C3%BAdio-usada-para-exibi%C3%A7%C3%A3o-produto-e-site-templatebackground-exibir-o-254939223.jpg",
+      description: "Descrição do artigo 3",
+      href: "#",
+    },
+    {
+      title: "Artigo 4",
+      image: "https://thumbs.dreamstime.com/b/wallpaper-de-fundo-cinza-claro-abstrato-vazio-sala-est%C3%BAdio-usada-para-exibi%C3%A7%C3%A3o-produto-e-site-templatebackground-exibir-o-254939223.jpg",
+      description: "Descrição do artigo 4",
+      href: "#",
+    },
+  ];
+
+  return (
+    <Carousel
+      opts={{
+        align: "start",
+        loop: true,
+      }}
+      className=""
+    >
+      <CarouselContent>
+        {articles.map((article, index) => (
+          <CarouselItem key={index} className="basis-1/3">
+            <CarrosselCard props={article} />
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselNext />
+      <CarouselPrevious />
+    </Carousel>
+  );
+};
+
+export default Carrossel;
--- desafio-5/src/app/artigos/page.tsx
+++ desafio-5/src/app/artigos/page.tsx
@@ -1,8 +1,15 @@
+import Hero from "./_components/hero";
+import Carrossel from "./_components/carousel";
 
 const Artigos = () => {
   return (
-    <div>Artigos</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <div className="max-w-7xl w-full px-4 py-8 flex flex-col items-center">
+        <Hero />
+        <Carrossel />
+      </div>
+    </div>
+  );
+};
 
-export default Artigos
+export default Artigos;
