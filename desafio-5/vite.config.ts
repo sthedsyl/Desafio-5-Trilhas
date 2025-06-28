159a7213a7e28433cb6994707e64a5aba4e682af
Sthefane <sthefanedias04@gmail.com>
2025-06-27 22:12:49 -0300

    chore: instalação e implementação do shadcn

--- desafio-5/vite.config.ts
+++ desafio-5/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,6 +9,10 @@ export default defineConfig({
     tailwindcss(),
     // This plugin is used to process Tailwind CSS styles
     react()
-  
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
