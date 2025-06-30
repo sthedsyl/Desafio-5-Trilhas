4fbff11cfe26f375a34a48b6a2c094f4f552d54d
alice torres <torresalice.dev@gmail.com>
2025-06-30 13:50:37 -0300

    criação: pagina criar artigo e usuario

---
+++ desafio-5/src/app/perfil/criar-artigo/page.tsx
@@ -0,0 +1,12 @@
+import Sidebar from '../componentes/Sidebar';
+
+const CriarArtigo = () => {
+  return (
+    <div>
+      <Sidebar />
+      <h1>Criar Artigo</h1>
+    </div>
+  )
+}
+
+export default CriarArtigo;
---
+++ desafio-5/src/app/perfil/criar-usuario/page.tsx
@@ -0,0 +1,12 @@
+import Sidebar from '../componentes/Sidebar';
+
+const CriarUsuario = () => {
+  return (
+    <div>
+      <Sidebar />
+      <h1>Criar Usuário</h1>
+    </div>
+  )
+}
+
+export default CriarUsuario;
