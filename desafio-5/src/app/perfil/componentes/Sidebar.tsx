707845f61ec84de84ec3aac21647500288d0b858
alice torres <torresalice.dev@gmail.com>
2025-06-29 23:42:23 -0300

    criação e estilo de sidebar

---
+++ desafio-5/src/app/perfil/componentes/Sidebar.tsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+import styles from './Sidebar.module.css';
+import { LayoutDashboard, FileText, Users } from 'lucide-react';
+
+const Sidebar = () => {
+  return (
+    <div className={styles.sidebar}>
+      <nav className={styles.menu}>
+        <Link to="/admin" className={styles.menuItem}>
+          <LayoutDashboard size={20} />
+          Visão Geral
+        </Link>
+        <Link to="/perfil/gerenciador-de-artigo" className={styles.menuItem}>
+          <FileText size={20} />
+          Gerenciar Artigos
+        </Link>
+        <Link to="/perfil/user" className={styles.menuItem}>
+          <Users size={20} />
+          Gerenciar Usuários
+        </Link>
+      </nav>
+    </div>
+  );
+};
+
+export default Sidebar;
